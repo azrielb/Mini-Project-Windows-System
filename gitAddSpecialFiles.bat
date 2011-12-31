667f23fb1f50099c83162a3185c61a43201776d5
Azriel Berger - HP620 <azrielb@gmail.com>
2012-01-01 01:11:33 +0200

    01:11

---
+++ gitAddSpecialFiles.bat
@@ -0,0 +1,12 @@
+@echo off
+git add *.bat
+git add *.cache
+git add *ssemblyInfo.cs
+git add *.csproj
+git add *.dll
+git add *.exe
+git add *.exe.*
+git add *.pdb
+git add *.sln
+git add *.suo
+git add *.txt
