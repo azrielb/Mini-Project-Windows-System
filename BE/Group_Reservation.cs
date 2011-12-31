a2b7d734c0b280884367cd2faf96e5a7de3cbf91
Azriel Berger - HP620 <azrielb@gmail.com>
2012-01-01 01:14:02 +0200

    Add Group-Reservation class

---
+++ BE/Group_Reservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BE
+{
+    class Group_Reservation : Reservation
+    {
+         //Field
+        private List<Room> rooms;
+        //Property
+        //Override Property
+        public override uint Price
+        {
+            get { return 0;/* (room.BedPrice == 0 ? room.Price : room.BedPrice) * days; */}
+        }
+
+        //Constructor
+        public Group_Reservation(uint ID, Tour_Agency agency, DateTime arrivalDate, Room room, uint days = 1)
+            : base(ID, agency, arrivalDate, days, 1)
+        {
+            //this.room = room;
+        }
+
+   }
+}
--- tests/Program.cs
+++ tests/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace tests
 {
@@ -11,7 +8,7 @@ namespace tests
         {
             BE.Room room = new BE.Room(1,3,100,40);
             BE.Tour_Agency tourAgency = new BE.Tour_Agency(1, "Reuven Tour LTD", "Reuven", BE.AgencyType.in_hotel_chain_ownership);
-            BE.Single_Reservation singleReservation = new BE.Single_Reservation(1, tourAgency, DateTime.Today, room);
+            BE.Single_Reservation singleReservation = new BE.Single_Reservation(1, tourAgency, DateTime.Today, room, 3);
             Console.WriteLine(room);
             Console.WriteLine(tourAgency);
             Console.WriteLine(singleReservation);
