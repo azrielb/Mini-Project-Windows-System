6ffdfd92ee79a7204eda29678c0648ae5bc2b38a
Azriel Berger - HP620 <azrielb@gmail.com>
2011-12-31 19:53:23 +0200

    Start project.

---
+++ BE/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BE
+{
+    class Room
+    {
+        private readonly uint roomID;
+
+        public uint RoomID
+        {
+            get { return roomID; }
+        } 
+
+    }
+}
