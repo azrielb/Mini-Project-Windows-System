aa1d9c89bb09a8242d55757eb7941bfbb973e1ac
Azriel Berger - HP620 <azrielb@gmail.com>
2012-01-01 00:17:15 +0200

    tests file

---
+++ tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BE.Room room = new BE.Room(1,3,100,40);
+            BE.Tour_Agency tourAgency = new BE.Tour_Agency(1, "Reuven Tour LTD", "Reuven", BE.AgencyType.in_hotel_chain_ownership);
+            BE.Single_Reservation singleReservation = new BE.Single_Reservation(1, tourAgency, DateTime.Today, room);
+            Console.WriteLine(room);
+            Console.WriteLine(tourAgency);
+            Console.WriteLine(singleReservation);
+        }
+
+    }
+}
