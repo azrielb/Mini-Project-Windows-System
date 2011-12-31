e5369ca55c450e80195262ada4185cf44fd474aa
Azriel Berger - HP620 <azrielb@gmail.com>
2012-01-01 00:15:19 +0200

    Add Single-Reservation class

---
+++ BE/Single_Reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BE
+{
+    public class Single_Reservation : Reservation
+    {
+        //Field
+        private Room room;
+        //Property
+        public Room Room
+        {
+            get { return room; }
+            //set { room = value; }
+        }
+        //Override Property
+        public override uint Price
+        {
+            get { return (room.BedPrice == 0 ? room.Price : room.BedPrice) * days; }
+        }
+
+        //Constructor
+        public Single_Reservation(uint ID, Tour_Agency agency, DateTime arrivalDate, Room room, uint days = 1)
+            : base(ID, agency, arrivalDate, days, 1)
+        {
+            this.room = room;
+        }
+
+    }
+}
