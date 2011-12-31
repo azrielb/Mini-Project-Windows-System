b625da4ad1de8e243ec2dbb1f24ae5ebcfa0fffe
Azriel Berger - HP620 <azrielb@gmail.com>
2012-01-01 00:16:26 +0200

    Add Azriel Berger Classes

---
+++ ABClasses/Checkings.cs
@@ -0,0 +1,19 @@
+﻿namespace ABClasses
+{
+    class Checkings
+    {
+        //This functions takes a number, and return whether the number is a valid israeli ID num.
+        public static bool CheckIdNum(ulong idnum)
+        {
+            if (idnum == 0) return false;
+            int sum = 0;
+            for (; idnum > 0; idnum /= 100)
+            {
+                int num = (int)(idnum % 100);
+                int lefter = (num / 10) * 2;
+                sum += (num % 10) + lefter - (lefter > 9 ? 9 : 0);
+            }
+            return (sum % 10 == 0);
+        }
+    }
+}
---
+++ ABClasses/EventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ABClasses
+{
+    //This class was taken from http://blogs.microsoft.co.il/blogs/shlomo/archive/2008/11/19/eventargs-lt-t-gt.aspx, with some changes. 
+    //This is a good idea! (see the link).
+    class EventArgs<T> : EventArgs
+    {
+        //One field
+        private readonly T data;
+
+        //One property
+        public T Data
+        {
+            get { return data; }
+        }
+
+        //Constructor
+        public EventArgs(T data)
+        {
+            this.data = data;
+        }
+
+    }
+}
