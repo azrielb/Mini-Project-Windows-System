052e8780a73b7f0d78a4a15a04514fe2ff1a5337
Azriel Berger - HP620 <azrielb@gmail.com>
2012-01-11 13:28:34 +0200

    Factories

---
+++ DAL/FactoryDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BE;
+
+namespace DAL {
+    public class FactoryDAL {
+        public static Idal<List<Room>, List<Tour_Agency>, List<Reservation>> getDAL() {
+            return new Dal_imp();
+        }
+    }
+}
