00e59d5ab4af9843d9117a20a4d369a433c1fd8b
Azriel Berger - HP620 <azrielb@gmail.com>
2012-01-15 14:03:57 +0200

    change field to propg in EventArgs<T>

--- ABClasses/EventArgs.cs
+++ ABClasses/EventArgs.cs
@@ -4,18 +4,12 @@ namespace ABClasses {
     //This class was taken from http://blogs.microsoft.co.il/blogs/shlomo/archive/2008/11/19/eventargs-lt-t-gt.aspx, with some changes. 
     //This is a good idea! (see the link).
     class EventArgs<T> : EventArgs {
-        //One field
-        private readonly T data;
-
         //One property
-        public T Data {
-            get { return data; }
-        }
+        public T Data { get; private set; }
 
         //Constructor
         public EventArgs(T data) {
-            this.data = data;
+            Data = data;
         }
-
     }
 }
